10e4877ce53337a46f705f69c17f8ceae8ec28ca
Azathot11 <azathotkamdoum@gmail.com>
2023-07-26 15:00:14 +0100

    high score

--- scoreboard.py
+++ scoreboard.py
@@ -12,18 +12,26 @@ class Scoreboard(Turtle):
         # self.write(f'Score: {self.score}', align='center', font=('Arial', 24, 'normal'))
         self.hideturtle()
         self.update_score()
+        self.get_highestScore()
+
+    def get_highestScore(self):
+        with open('data.txt') as file:
+            self.highest_score = int(file.read())
+        self.update_score()
 
     def update_score(self):
         self.clear()
         self.write(f'Score: {self.score} High score {self.highest_score}', align='center', font=('Arial', 24, 'normal'))
-        self.score += 1
 
     def reset(self):
         if self.score > self.highest_score:
             self.highest_score = self.score
+            with open('data.txt', mode='w') as file:
+                file.write(str(self.highest_score))
         self.score = 0
         self.update_score()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write('GAME OVER', align='center', font=('Arial', 24, 'normal'))
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
+
